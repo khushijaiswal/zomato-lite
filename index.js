9ccc587cd80f36c8e60cb4f3e59131bb12378368
khushijaiswal <jaiswalkhushi9011@gmail.com>
2025-01-11 11:13:59 +0530

    update-info

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors({
 
 app.use("/api/auth", require("./routes/auth.routes"))
 app.use("/api/resturant", resturantProtected, require("./routes/resturant.routes"))
-app.use("/api/costomer", customertProtected, require("./routes/customer.route"))
+app.use("/api/customer", customertProtected, require("./routes/customer.route"))
 
 app.use("*", (req, res) => {
     res.status(404).json({ message: "resource not found" })
