6c1389052631f24dfc79c33f6a2f5b70ddba6897
khushijaiswal <jaiswalkhushi9011@gmail.com>
2025-01-25 10:25:46 +0530

    add rider logout socket

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -139,10 +139,9 @@ exports.updateAdminRider = asyncHandler(async (req, res) => {
 
 exports.updateRiderAccount = asyncHandler(async (req, res) => {
     const { rid } = req.params
-
     const result = await Rider.findByIdAndUpdate(rid, { isActive: req.body.isActive }, { new: true })
     if (!result.isActive) {
-        io.emit("account-block")
+        io.emit("rider-logout")
     }
     res.json({ message: "rider account uupdate success" })
 
